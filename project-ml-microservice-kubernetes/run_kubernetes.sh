b37fe40757d6b53da3b93c7ab9df3a8f0c4fb495
sakshi kataria <sakshi.kataria12@gmail.com>
2022-10-29 23:08:17 +0530

    Update run_kubernetes.sh

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sakshi781996/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy api --image=sakshi781996/api:v1.0.0 --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/api-6c9c8c5487-fx7ql --address 0.0.0.0 8000:8000
